5e8143a5ec6a06254976df8c2cdd5d264071e0b8
nairobi-j <jerinusrat001@gmail.com>
2024-06-04 23:17:19 +0600

    see

--- Basha-Vara/src/App.jsx
+++ Basha-Vara/src/App.jsx
@@ -7,28 +7,35 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className= "mainPage">
+     <div className='title'> <h1>Home Rental Help</h1></div> 
+     <div className="loginInput">
+     <div className="nameInput">
+      <p>Name:  <input type="name"></input></p>
+     
+     </div>
+     <div className="passwordInput">
+      <p>Password: <input type='password'></input></p>
+      
+     </div>
+     </div>
+      <button>Log in</button>
+      <div className="help">
+      <img  width= {25} height={48} src = "circle-info-solid.svg"/>
+      <p>Help</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="Aboutus">
+      <img  width= {25} height={48} src = "about.svg"/>
+      <p>About Us</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+     
+    
+    
+      
+    </div>
+
+
   )
 }
 
