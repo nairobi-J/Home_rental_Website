59d06449809fde708a24d94a35180fbaf0f957ec
nairobi-j <jerinusrat001@gmail.com>
2024-06-06 14:12:37 +0600

    lqlql

---
+++ Basha-Vara/src/components/AnimatedTextWord.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const AnimatedTextWord = ({ text }) => {
+  const words = text.split(" ");
+
+// Variants for Container of words.
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.4 * i },
+    }),
+  };
+
+// Variants for each word.
+
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 1,
+        stiffness: 10,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 2,
+      transition: {
+        type: "spring",
+        damping: 1,
+        stiffness: 10,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      style={{ overflow: "hidden", display: "flex" }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {words.map((word, index) => (
+        <motion.span
+          variants={child}
+          style={{ marginRight: "10px" }}
+          key={index}
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+
+export default AnimatedTextWord;
--- Basha-Vara/src/components/MainPage.jsx
+++ Basha-Vara/src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './MainPage.module.css'
 import {motion} from "framer-motion";
+import AnimatedTextWord from './AnimatedTextWord';
 const MainPage = () => {
   return (
     
@@ -8,23 +9,14 @@ const MainPage = () => {
         <div className={styles.mainImage}><img src = "images/Home-Rental-Logo.jpg"/></div>
      <div className='title' > <h1>HOME RENTAL HELP</h1></div> 
      <div className={styles.qoute}>
-      const text = "Framer Motion is a really cool tool".split(" ");
-    {/* {text.map((el, i) => (
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            duration: 0.25,
-            delay: i / 10
-          }}
-          key={i}
-        >
-          {el}{" "}
-        </motion.span>
-      ))} */}
-      <p>A Complete solution for Home renting. Where you can find your dream home without even roaming around road. Take a deep breath and dive into the latest helping hand of yours</p>
-      <p>Happy Renting Dude! We are always there for you. </p>
-     </div>
+      <AnimatedTextWord text="A Complete solution for Home renting. " />
+      <AnimatedTextWord text=" Where you can find your dream home without even roaming around road." />
+      <AnimatedTextWord text=" Take a deep breath and dive into the latest helping hand of yours" />
+      <p> </p>
+      <AnimatedTextWord text="Happy Renting Dude! We are always there for you." />
+      <p>     </p>
+    </div>
+   
      <button>Sign Up?</button>
      <p>Or</p>
      
