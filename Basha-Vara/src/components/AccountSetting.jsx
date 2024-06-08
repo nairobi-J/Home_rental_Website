7f3fd6fc9b598d171a4116cdb0ca172ac24f9e43
jessia <sadiajessia@gmail.com>
2024-06-09 00:54:40 +0600

    acc setting

--- Basha-Vara/src/components/AccountSetting.jsx
+++ Basha-Vara/src/components/AccountSetting.jsx
@@ -1,128 +1,118 @@
-import React, {useState, useRef} from 'react';
+import React, { useState } from 'react';
 import styles from './AccountSetting.module.css';
-import Header from './Header';
 
 const AccountSetting = () => {
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
 
-    const [image, setImage] = useState(null);
-    const hiddenFileInput = useRef(null);
-  
-    const handleImageChange = (event) => {
-      const file = event.target.files[0];
-      const imgname = event.target.files[0].name;
+  // Handle image selection
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
       const reader = new FileReader();
-      reader.readAsDataURL(file);
       reader.onloadend = () => {
-        const img = new Image();
-        img.src = reader.result;
-        img.onload = () => {
-          const canvas = document.createElement("canvas");
-          const maxSize = Math.max(img.width, img.height);
-          canvas.width = maxSize;
-          canvas.height = maxSize;
-          const ctx = canvas.getContext("2d");
-          ctx.drawImage(
-            img,
-            (maxSize - img.width) / 2,
-            (maxSize - img.height) / 2
-          );
-          canvas.toBlob(
-            (blob) => {
-              const file = new File([blob], imgname, {
-                type: "image/png",
-                lastModified: Date.now(),
-              });
-  
-              console.log(file);
-              setImage(file);
-            },
-            "image/jpeg",
-            0.8
-          );
-        };
+        setImagePreview(reader.result);
       };
-    };
-  
-    const handleUploadButtonClick = (file) => {
-      var myHeaders = new Headers();
-      const token = "adhgsdaksdhk938742937423";
-      myHeaders.append("Authorization", `Bearer ${token}`);
-  
-      var formdata = new FormData();
-      formdata.append("file", file);
-  
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: formdata,
-        redirect: "follow",
-      };
-  
-      fetch("https://trickuweb.com/upload/profile_pic", requestOptions)
-        .then((response) => response.text())
-        .then((result) => {
-          console.log(JSON.parse(result));
-          const profileurl = JSON.parse(result);
-          setImage(profileurl.img_url);
-        })
-        .catch((error) => console.log("error", error));
-    };
-  
-    const handleClick = (event) => {
-      hiddenFileInput.current.click();
-    };
-  
-    return ( 
-      <>
-      <Header/>
-      <div className={`${styles.accountSetting} container`}> 
-        <h2>Update Your Profile</h2>
-      <div className="image-upload-container">
-        <div className="box-decoration">
-          <label htmlFor="image-upload-input" className="image-upload-label">
-            {image ? image.name : "Select Profile Picture"}
-          </label>
-          <div onClick={handleClick} style={{ cursor: "pointer" }}>
-            {image ? (
-              <img src={URL.createObjectURL(image)} alt="upload image" className="img-display-after" />
-            ) : (
-              <img src="./photo.png" alt="upload image" className="img-display-before" />
-            )}
-  
-            <input
-              id="image-upload-input"
-              type="file"
-              onChange={handleImageChange}
-              ref={hiddenFileInput}
-              style={{ display: "none" }}
-            />
-          </div>
-  
-          <button
-            className="image-upload-button"
-            onClick={handleUploadButtonClick}
-          >
-            Upload
-          </button>
+      reader.readAsDataURL(file);
+    } else {
+      setImage(null);
+      setImagePreview(null);
+    }
+  };
+
+  return (
+    <div className={`${styles.signIn} container`}>
+    <div className="profile-container">
+      <h2>Profile Settings</h2>
+      <form>
+        {/* Profile Image */}
+        <div className="form-group">
+          <label htmlFor="profileImage">Profile Picture:</label>
+          <input
+            type="file"
+            id="profileImage"
+            name="profileImage"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
         </div>
-      </div>
-      <div className='otherInfo'>
-      <p>Username: <input type='username'></input></p>
-      <p>First Name: <input type='username'></input></p>
-      <p>Last Name: <input type='username'></input></p>
-      <p>Contact No. : <input type='contact'></input></p>
-      <p>Address: <input type='address'></input></p>
+        <br/>
+        {imagePreview && (
+          <div className="image-preview">
+            <img src={imagePreview} alt="Profile Preview" />
+          </div>
+        )}
+         <br/>
 
-      </div>
-      <div>
-        <button className='saveBtn'>Save</button>
-      </div>
-      </div>
+        {/* Other profile fields */}
+        <div className="form-group">
+          <label htmlFor="firstName">First Name: </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            placeholder="Update your first name"
+          />
+        </div>
+        <br/>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name: </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            placeholder="update your last name"
+          />
+        </div>
+        <br/>
+        <div className="form-group">
+          <label htmlFor="Contact">Contact No.: </label>
+          <input
+            type="contact"
+            id="contact"
+            name="contact"
+            placeholder="update your contact number"
+          />
+        </div>
+        <br/>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter old password"
+          />
+        </div>
+        <br/>
 
-      </>
-      
-    );
-  }
-  
+        <div className="form-group">
+          <label htmlFor="password">New Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="new password"
+            placeholder="Enter new password"
+          />
+        </div>
+
+        <br/>
+        <div className="form-group">
+          <label htmlFor="address">Address:</label>
+          <input
+            type="address"
+            id="address"
+            name="address"
+            placeholder="Update Address"
+          />
+        </div>
+        <br/>
+        <button type="button">Update Profile</button>
+      </form>
+    </div>
+    </div>
+  );
+};
 
 export default AccountSetting;
