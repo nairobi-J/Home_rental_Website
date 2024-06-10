edd54002daa168008960c1ff528ee01ace7dbb18
nairobi-j <jerinusrat001@gmail.com>
2024-06-10 14:22:30 +0600

    ddddd

--- Basha-Vara/src/components/Navbar.jsx
+++ Basha-Vara/src/components/Navbar.jsx
@@ -2,27 +2,41 @@ import React from 'react';
 import Header from './Header';
 import {IconButton} from '@mui/material'
 import {Search, Menu, Person} from '@mui/icons-material'
-
+import styles from './Navbar.module.css'
 const Navbar = () => {
   return (
     <>
     <Header/>
-    <div className = 'navbar'>
-        
-        <img src = "images/Logo.jpg"/>
+<div className = {`${styles.navbar} container`}>
+    <div className="logo">
+    <img src = "images/Logo.jpg"/>
+    </div>
+
+       
         <div className="navbar-search">
+      
           <input type= "text" placeholder='Search...'></input>
           <IconButton>
            <Search sx= {{}}/>
           </IconButton>
         </div>
-        <div className="navbar-right">
-           
-        <button className='navbar-right-ac'>
-            <Menu/>
-            <Person/>
-        </button>
+        
+           <nav >
+        
+        <div >
+            <ul className = {styles.navi}>
+               
+                <li><a href = "/about">About</a></li>
+                <li><a href = "/host">Become a Host</a></li>
+                <li><a href = "/wishList">WishList</a></li>
+                <li><a href = "/accountSetting" >Settings</a></li>
+     
+                
+            </ul>
         </div>
+    </nav>
+       
+       
       
       </div>
     </>
