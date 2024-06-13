84da6727c71c50758e7a4ed3bca854d37031dfa8
jessia <sadiajessia@gmail.com>
2024-06-13 21:03:28 +0600

    login auth successful

--- Basha-Vara/src/components/SignIn.jsx
+++ Basha-Vara/src/components/SignIn.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './SignIn.module.css';
+import axios from 'axios';
 import Header from './Header';
 import Navbar from './Navbar';
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/login', { email, password })
+      .then(result => {
+        console.log(result);
+        if (result.data === "Login Successful!") {
+          handleNavigation('/homePage');
+        } else {
+          console.log(result.data); 
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  
+
+
   const navigate = useNavigate(); // Initialize the navigate function
 
   // Function to handle navigation
@@ -17,17 +37,41 @@ const SignIn = () => {
       <div className={`${styles.signIn} container`}>
       <Navbar/>
         <h2>Login To Your Account!</h2>
-        <div className='loginInfo'>
-          <p>Name: <input type='Name' /></p>
-          <p>Email: <input type='Email' /></p>
-          <p>Password: <input type='Password' /></p>
-        </div>
-        <div>
-        <button onClick={() => handleNavigation('/homePage')}>Log in</button>
-        </div>
+        <form onSubmit={handleSubmit} method='POST'>
+       
+        <div className="loginInfo">
+      <label htmlFor='email'>
+        
+        </label>
+      <p>Email:  <input
+                   type="mail"
+        
+                  autoComplete='off'
+                  name='email'
+                   placeholder='Enter Email'
+                   onChange={(e) => setEmail(e.target.value)}
+                   />
+      </p>
+      </div>
+      <div className="loginInfo">
+      <label htmlFor='password'>
+          
+        </label>
+      <p>Password:  <input type="password"
+                           autoComplete='off'
+                           name='password'
+                           placeholder='Enter a Strong Password'
+                           onChange={(e) => setPassword(e.target.value)}
+                           />
+      </p>
+      </div>
+
+        <button type='submit'>Log in</button>
+        
         <div>
           <a href='./forgotPassword'>Forgot Password?</a>
         </div>
+        </form>
       </div>
     </>
   );
