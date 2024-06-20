0acaa111ddfea5a942bd3b3631cf3f367faedc5e
nairobi-j <jerinusrat001@gmail.com>
2024-06-20 21:53:59 +0600

    auth, catche

---
+++ Basha-Vara/src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import state from "./state"; // Assuming 'state' is the root reducer
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, state);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production', // Enable Redux DevTools in development
+});
+
+export let persistor = persistStore(store);
--- server/app.js
+++ server/app.js
@@ -1,51 +1,27 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const registerModel = require('./models/Dream_Home');
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-dotenv.config();
-
-// console.log(process.env.DBURL)
-
-// mongoose.connect("mongodb+srv://sadiajessia:sadia@1234@sadiajessia.sjy4gkn.mongodb.net/dream-home-bd");
 
 
-mongoose.connect(process.env.DBURL)
-  .then(() => {
-    console.log("Successfully connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  });
 
-app.post('/register', (req, res) => {
-    registerModel.create(req.body)
-    .then(register => res.json(register))
-    .catch(err => res.json(err))
+
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const cors = require("cors");
+
+const authRoutes = require('./routes/auth.js')
+//const registerModel = require('./routes/auth.js');
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use("/auth", authRoutes)
+const PORT = 3000
+mongoose.connect(process.env.DBURL,{
+    dbName :"dream-home",
+    useNewUrlParser: true,
+
+    useUnifiedTopology:true
 })
-
-app.post('/login', (req, res) => {
-    const {email, password} = req.body
-    registerModel.findOne({email: email})
-    .then(user => {
-        if(user){
-        if(user.password === password){
-            res.json("Login Successful!")
-        } else{
-            res.json("The password is incorrect!")
-        }
-    }else{
-        res.json("No such record existed!")
-    }
-    })
-})
-
-app.listen(3001, () => {
-    console.log("server running on port 3001");
-});
+.then(()=>{
+    app.listen(PORT, ()=> console.log(`server Port : ${PORT}`))
+}).catch((err) => console.log(`${err} error`))
