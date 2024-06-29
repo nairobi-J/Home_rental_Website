6e3aa12aa9c3a188cd831d6ace53e9b83fe5917d
nairobi-j <jerinusrat001@gmail.com>
2024-06-29 16:23:02 +0600

    just initiated homepage

---
+++ Basha-Vara/src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//while we load it will take time to load is that why loader
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <div className="loader-inner">
+            
+        </div>
+      
+    </div>
+  );
+}
+
+export default Loader;
