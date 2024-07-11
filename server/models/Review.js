d16207ce08de513555491878551834a14021e8cb
jessia <sadiajessia@gmail.com>
2024-07-11 22:43:01 +0600

    added username in review

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,4 +1,36 @@
 
+// import mongoose from 'mongoose';
+// const { Schema } = mongoose;
+
+// const reviewSchema = new Schema({
+//   listingId: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'Listing',
+//     required: true
+//   },
+//   userId: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'User',
+//     required: true
+//   },
+//   rating: {
+//     type: Number,
+//     required: true,
+//     min: 1,
+//     max: 5
+//   },
+//   comment: {
+//     type: String,
+//     required: true
+//   }
+// }, { timestamps: true });
+
+// const Review = mongoose.model('Review', reviewSchema);
+
+// export default Review;
+
+// Review model (/models/Review.js)
+
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
@@ -6,23 +38,23 @@ const reviewSchema = new Schema({
   listingId: {
     type: Schema.Types.ObjectId,
     ref: 'Listing',
-    required: true
+    required: true,
   },
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
   },
   rating: {
     type: Number,
     required: true,
     min: 1,
-    max: 5
+    max: 5,
   },
   comment: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 }, { timestamps: true });
 
 const Review = mongoose.model('Review', reviewSchema);
