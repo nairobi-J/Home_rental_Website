d6ceeda5e7ffa63035c8161ffbcc89b719551795
nairobi-j <jerinusrat001@gmail.com>
2024-10-13 17:33:30 +0600

    Changed the layout of home page , added some important data solved some navigation related issues changed some listing components

--- Basha-Vara/src/components/ListingCard.jsx
+++ Basha-Vara/src/components/ListingCard.jsx
@@ -29,37 +29,40 @@ const ListingCard = ({
   };
 
   return (
-    <div className={`${Styles['listing-card']} container`}>
-      <div className={`${Styles['image-slider']} container`}>
-        <div className={`${Styles.slider} container`} style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-          {listingPhotoPaths?.map((item, index) => (
-            <div className={`${Styles.slide} container`} key={index}>
-              <img src={`http://localhost:3000/${item.replace("public", "")}`} alt={`photo ${index + 1}`} />
-              <div className={`${Styles['prev-button']} container`} onClick={goToPrevSlide}>
-                <ArrowBackIosNew sx={{ fontSize: "15px" }} />
+    <div className={Styles['listing-card-container']}>
+      <div className={Styles['listing-card']}>
+        <div className={Styles['image-slider']}>
+          <div className={Styles.slider} style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {listingPhotoPaths?.map((item, index) => (
+              <div className={Styles.slide} key={index}>
+                <img src={`http://localhost:3000/${item.replace('public', '')}`} alt={`photo ${index + 1}`} />
               </div>
-              <div className={`${Styles['next-button']} container`} onClick={goToNextSlide}>
-                <ArrowForwardIos sx={{ fontSize: "15px" }} />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className={Styles['prev-button']} onClick={goToPrevSlide}>
+            <ArrowBackIosNew sx={{ fontSize: '15px' }} />
+          </div>
+          <div className={Styles['next-button']} onClick={goToNextSlide}>
+            <ArrowForwardIos sx={{ fontSize: '15px' }} />
+          </div>
+        </div>
+
+        <div className={Styles.card}>
+          <h3>
+            Address: {thana}, {city}, {district}
+          </h3>
+          <p>Category: {category}</p>
+          <p>Type: {type}</p>
+          <p>
+            <span>
+              Rent: <FaBangladeshiTakaSign />
+              {price}
+            </span>{' '}
+            /mo
+          </p>
+          <button onClick={handleClick}>See details</button>
         </div>
       </div>
-
-    <div  className={`${Styles['card']} container`}> 
-    <h3>Address: {thana},{city}, {district}</h3>
-      <p>Category: {category}</p>
-      <p>Type: {type}</p>
-      <p>
-        <span>Rent:
-          <FaBangladeshiTakaSign />{ price}
-        </span>
-         /mo
-      </p>
-      <button onClick={handleClick}>See details</button>
-    </div>
-     
-      
     </div>
   );
 };
