69c832de06a0470ab1ea4738385a39ed05c50577
nairobi-j <jerinusrat001@gmail.com>
2024-10-13 18:06:36 +0600

    sign in and sign up page
    added extra two buttons in this to pages to navigate smoothly

--- Basha-Vara/src/components/SignIn.jsx
+++ Basha-Vara/src/components/SignIn.jsx
@@ -1,3 +1,94 @@
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import styles from './SignIn.module.css';
+// import Header from './Header';
+// import Navbar from './Navbar';
+// import { setLogin } from '../redux/userSlice';
+// import { useDispatch } from 'react-redux';
+
+// const SignIn = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch("http://localhost:3000/auth/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ email, password }),
+//       });
+  
+//       if (!response.ok) {
+//         const error = await response.json();
+//         console.log('Error:', response.status, error.message);
+//         if (response.status === 409) {
+//           console.log("User doesn't exist");
+//         } else if (response.status === 400) {
+//           console.log('Wrong password');
+//         } else {
+//           console.log('An unknown error occurred');
+//         }
+//         return;
+//       }
+  
+//       const loggedIn = await response.json();
+      
+//       // Store userId in LocalStorage
+//       localStorage.setItem('userId', loggedIn.user._id);
+
+//       dispatch(setLogin({
+//         user: loggedIn.user,
+//         token: loggedIn.token,
+//       }));
+//       navigate('/');
+//     } catch (err) {
+//       console.log('Login failed', err.message);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <Header />
+//       <Navbar />
+//       <div className={styles.container}>
+//         <div className={styles.signIn}>
+//           <form onSubmit={handleSubmit}>
+//             <div className={styles.email}>
+//               <input
+//                 type="email"
+//                 placeholder='Email'
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 required
+//               />
+//             </div>
+//             <div className={styles.password}>
+//               <input
+//                 type="password"
+//                 placeholder='Password'
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 required
+//               />
+//             </div>
+//             <div className={styles.login}>
+//               <button type='submit'>Log in</button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SignIn;
+
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './SignIn.module.css';
@@ -50,6 +141,9 @@ const SignIn = () => {
       console.log('Login failed', err.message);
     }
   };
+  const handleClick = () => {
+    navigate('/signUp');
+  }
 
   return (
     <>
@@ -78,8 +172,12 @@ const SignIn = () => {
             </div>
             <div className={styles.login}>
               <button type='submit'>Log in</button>
+
             </div>
           </form>
+          <p>Not registered?
+
+          </p> <button onClick = {handleClick}>Reg here</button>
         </div>
       </div>
     </>
