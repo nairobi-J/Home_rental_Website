5049fc1336b671cf79abc0d32420096a2e383f87
nairobi-j <jerinusrat001@gmail.com>
2024-11-09 03:33:26 +0600

    changed some design in listing details

--- Basha-Vara/src/components/ListingDetails.jsx
+++ Basha-Vara/src/components/ListingDetails.jsx
@@ -5,6 +5,7 @@ import Loader from './Loader';
 import Review from './Review';
 import FileUpload from './FileUpload';
 import styles from './ListingDetails.module.css';
+import Navbar from './Navbar';
 
 const ListingDetails = () => {
     const [loading, setLoading] = useState(true);
@@ -80,66 +81,83 @@ const ListingDetails = () => {
     };
 
     return (
-        <div className={styles['listing-details']}>
-            <div className={styles['title']}>
-                <h1>{listing.title}</h1>
-            </div>
-            <div className={styles['photos']}>
-                {listing.listingPhotoPaths?.map((item, index) => (
-                    <img key={index} src={`http://localhost:3000/${item.replace("public", "")}`} alt={`listing photo ${index + 1}`} />
-                ))}
-            </div>
-            <h2>{listing.type} in {listing.thana}, {listing.district}, {listing.city}</h2>
-            <p>{listing.personCount} Person (preferred). {listing.bedroomCount} Bedroom. {listing.bathroomCount} Bathroom. {listing.balconyCount} Balcony</p>
-            <hr />
-            <div className={styles['creator']}>
-                {creator && creator.profileImagePath ? (
-                    <>
-                        <img src={`http://localhost:3000/${creator.profileImagePath.replace("public", "")}`} alt="creator" />
-                        <h3>Created by {creator.name}</h3>
-                    </>
-                ) : (
-                    <p>Creator information not available.</p>
-                )}
-            </div>
-            <hr />
-            <h3>Description</h3>
-            <p>{listing.description}</p>
-            <hr />
-            <h3>{listing.highlight}</h3>
-            <p>{listing.highlightDetails}</p>
-            <div className={styles['rent']}>
-                <div>
-                    <h2>What this place offers</h2>
-                    <div className={styles['amenities']}>
-                        {listing.amenity?.map((item, index) => (
-                            <div className={styles['amenity']} key={index}>
-                                <div className={styles['facility-icon']}>
-                                    {/* Ensure the facilities and icons are mapped correctly */}
-                                </div>
-                                <p>{item}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <Review listingId={listingId} />
-                <div className={styles['price-calculation']}>
-                    <label>
-                        How Many Months you want to give advance? (at least one month needed):
-                        <input type="number" value={multiplier} onChange={handleMultiplierChange} />
-                    </label>
-                    <p>Price: {listing.price} x {multiplier} = {listing.price * multiplier}</p>
-                </div>
-                <h3>add your national id</h3>
-                <FileUpload onUploadSuccess={handleUploadSuccess} />
-                <button className={styles['button']} type="submit" onClick={handleSubmit}>
-                    Rent
-                </button>
-                <p>When you click on the rent your apartment will be confirmed</p>
-            </div>
-            <hr />
+   
+      <>
+        <div className={styles['list']}>
+          
+          <div>
+            <p>Here is the details:</p>
+          </div>
+       
+       <div className={styles['listing-details']}>
            
-        </div>
+           <div className={styles['title']}>
+               <h1>Title:{listing.title}</h1>
+           </div>
+           <div className={styles['photos']}>
+               <h1>Photos:</h1>
+               {listing.listingPhotoPaths?.map((item, index) => (
+                   <img key={index} src={`http://localhost:3000/${item.replace("public", "")}`} alt={`listing photo ${index + 1}`} />
+               ))}
+           </div>
+           <h2>{listing.type} in {listing.thana}, {listing.district}, {listing.city}</h2>
+           <p>Person(preferred): {listing.personCount} </p>
+            <p> Bedroom: {listing.bedroomCount}</p>
+            <p>Bathroom: {listing.bathroomCount}</p>
+            <p>Balcony: {listing.balconyCount} </p>
+            <p></p>
+           <hr />
+           <div className={styles['creator']}>
+               {creator && creator.profileImagePath ? (
+                   <>
+                       <img src={`http://localhost:3000/${creator.profileImagePath.replace("public", "")}`} alt="creator" />
+                       <h3>Created by {creator.name}</h3>
+                   </>
+               ) : (
+                   <p>Creator information not available.</p>
+               )}
+           </div>
+           <hr />
+           <h3>Description</h3>
+           <p>{listing.description}</p>
+           <hr />
+           <h3>{listing.highlight}</h3>
+           <p>{listing.highlightDetails}</p>
+           <div className={styles['rent']}>
+               <div>
+                   <h2>What this place offers</h2>
+                   <div className={styles['amenities']}>
+                       {listing.amenity?.map((item, index) => (
+                           <div className={styles['amenity']} key={index}>
+                               <div className={styles['facility-icon']}>
+                                   {/* Ensure the facilities and icons are mapped correctly */}
+                               </div>
+                               <ol type=''>{item}</ol>
+                           </div>
+                       ))}
+                   </div>
+               </div>
+               <Review listingId={listingId} />
+               <div className={styles['price-calculation']}>
+                   <label>
+                       How Many Months you want to give advance? (at least one month needed):
+                       <input type="number" value={multiplier} onChange={handleMultiplierChange} />
+                   </label>
+                   <p>Price: {listing.price} x {multiplier} = {listing.price * multiplier}</p>
+               </div>
+               <h3>add your national id</h3>
+               <FileUpload onUploadSuccess={handleUploadSuccess} />
+               <button className={styles['button']} type="submit" onClick={handleSubmit}>
+                   Rent
+               </button>
+               <p>When you click on the rent your apartment will be confirmed</p>
+           </div>
+           <hr />
+          
+       </div> 
+       </div>
+      </>
+        
     );
 };
 
